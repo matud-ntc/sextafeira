28545714ab92c9e3e836a1ca1604ab1219fafb3c
matud_ntc <matudntc@gmail.com>
2020-04-04 13:31:57 -0700

    no message

--- src/App.js
+++ src/App.js
@@ -37,8 +37,6 @@ class App extends Component {
       </BrowserView>
       <MobileView>
     <div style={{marginTop:0, marginLeft:0, marginRight:0, width:'100%', height:220, backgroundColor: '#E3E3E3', boxShadow:'0 4px 8px rgba(255, 231, 231, 0.6)', zIndex: 2}}>
-    <h1> This is rendered only on mobile </h1>
-
         <img src={logo} alt="logo" style={{height:200, width:200, marginRight:350, marginLeft:150, minWidth:40, minHeight:40}}></img>
         <iframe width="300" height="170px"  frameborder="0" seamless  src="//www.caster.fm/widgets/em_player.php?uid=517083&c=000000&t=color"><p>Your browser does not support iframes.</p></iframe>
       </div>
