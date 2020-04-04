5d1c68b8e00c4c0e37e0b34126a10bd3527e6d0a
matud_ntc <matudntc@gmail.com>
2020-04-04 13:29:23 -0700

    no message

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ import Iframe from 'react-iframe';
 import { FacebookProvider, Comments } from 'react-facebook';
 import {Helmet} from 'react-helmet';
 import font from './fonts/Montserrat-Bold.ttf';
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
+
 class App extends Component {
   
   render(){
 
     return (
-      <div style={{backgroundColor: '#F1F1F1', 
+      <div style={{backgroundColor: '#393939', 
         width:'100%',
         height:'100%',
         top:'0px',
@@ -20,13 +27,22 @@ class App extends Component {
         left:'0px'
       }}>  
       <Helmet>
-        <style>{'body { background-color: #F1F1F1; }'}</style>
+        <style>{'body { background-color: #393939; }'}</style>
       </Helmet>
-      <div style={{marginTop:0, marginLeft:0, marginRight:0, width:'100%', height:220, backgroundColor: '#E3E3E3', boxShadow:'0 4px 8px rgba(0, 0, 0, 0.2)', zIndex: 2, position: 'fixed'}}>
-        <img src={logo} alt="logo" style={{height:200, width:200, marginRight:150}}></img>
+      <BrowserView>
+      <div style={{marginTop:0, marginLeft:0, marginRight:0, width:'100%', height:220, backgroundColor: '#E3E3E3', boxShadow:'0 4px 8px rgba(255, 231, 231, 0.6)', zIndex: 2, position: 'fixed'}}>
+        <img src={logo} alt="logo" style={{height:200, width:200, marginRight:350, marginLeft:150, minWidth:40, minHeight:40}}></img>
         <iframe width="300" height="170px"  frameborder="0" seamless  src="//www.caster.fm/widgets/em_player.php?uid=517083&c=000000&t=color"><p>Your browser does not support iframes.</p></iframe>
       </div>
+      </BrowserView>
+      <MobileView>
+    <div style={{marginTop:0, marginLeft:0, marginRight:0, width:'100%', height:220, backgroundColor: '#E3E3E3', boxShadow:'0 4px 8px rgba(255, 231, 231, 0.6)', zIndex: 2}}>
+    <h1> This is rendered only on mobile </h1>
 
+        <img src={logo} alt="logo" style={{height:200, width:200, marginRight:350, marginLeft:150, minWidth:40, minHeight:40}}></img>
+        <iframe width="300" height="170px"  frameborder="0" seamless  src="//www.caster.fm/widgets/em_player.php?uid=517083&c=000000&t=color"><p>Your browser does not support iframes.</p></iframe>
+      </div>
+</MobileView>
         <div style={{ height: '500px',
             marginLeft: 'auto',
             marginRight: 'auto',
@@ -34,8 +50,10 @@ class App extends Component {
             minWidth: '300px',
             paddingTop:600
               }}>
+                      <h2 style={{color:'#E3E3E3'}}>Dejanos tu comentario</h2>
+
           <FacebookProvider appId="895498000924043" style={{ }}>
-          <Comments orderBy="reverse_time" numPosts="5" href="https://www.facebook.com/Testestest-100720924932190" />
+          <Comments colorScheme="dark" orderBy="reverse_time" numPosts="5" href="https://www.facebook.com/Testestest-100720924932190" />
           </FacebookProvider>
         </div>
       
