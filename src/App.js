b0fd7febe3e91ef44b3c6ed7b80a00263aeb86df
matud_ntc <matudntc@gmail.com>
2020-04-26 22:46:50 -0700

    no message

--- src/App.js
+++ src/App.js
@@ -3,31 +3,23 @@ import './App.css';
 import logo from './Images/bg.png';
 import slogan from './Images/slogan.png';
 import astro from './Images/astro.png';
-import Iframe from 'react-iframe';
 import { FacebookProvider, Comments } from 'react-facebook';
 import {Helmet} from 'react-helmet';
-import font from './fonts/Montserrat-Bold.ttf';
 import { Divider, Tab } from 'semantic-ui-react'
-import MediaQuery from 'react-responsive'
 
 import {
   BrowserView,
   MobileView,
-  isBrowser,
-  isMobile
 } from "react-device-detect";
 import 'react-tabs/style/react-tabs.css';
 import { makeStyles } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import ReactGA from 'react-ga';
 import Typography from '@material-ui/core/Typography';
-
+import  ReactCardCarousel from 'react-card-carousel';
 const colors = [
   'red',
 ]
@@ -127,58 +119,239 @@ class App extends Component {
         height:'100%',
         top:'0px',
         zIndex:1000,
+        background: '#f8f9fb',
         left:'0px'
       }}>  
         <link rel="stylesheet" href="https://use.typekit.net/vnr7wcv.css"/>
         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
 
         <Helmet>
-          <style>{'body { background-color: #373737; }'}</style>
+          <style>{'body { background-color: #1b1b1b; }'}</style>
         </Helmet>
         <BrowserView>
-          <div style={{marginTop:0, marginLeft:0, marginRight:0, width:'100%', height:190, backgroundColor: '#e3e3e3', boxShadow:'0 4px 8px rgba(255, 231, 231, 0.6)', zIndex: 2, position: 'fixed'}}>
-            <img src={logo} alt="logo" style={{height:120, width:270, marginRight:180, marginLeft:90, minWidth:40, minHeight:40}}></img>
-            <iframe marginTop="10" width="300" height="170"  frameborder="0" seamless  src="//www.caster.fm/widgets/em_player.php?uid=517083&c=000000&t=color"><p>Your browser does not support iframes.</p></iframe>
-            <img src={slogan} alt="logo" style={{height:100, width:400, marginLeft: 90, marginBottom:27, minWidth:40, minHeight:40}}></img>
+          <div style={{marginTop:0, marginLeft:0, marginRight:0, width:'100%', height:160, backgroundColor: '#effe05', boxShadow:'0 4px 8px rgba(255, 231, 231, 0.6)', zIndex: 2}}>
+            <img src={logo} alt="logo" style={{height:100, marginTop:0, width:250, marginRight:180, marginLeft:90, minWidth:40, minHeight:40}}></img>
+            <img src={slogan} alt="logo" style={{height:100, marginTop:0,  width:400, marginLeft: 90, marginBottom:27, minWidth: 10, minHeight:10}}></img>
           </div>
-          <div style={{ height: '600px',
-              marginLeft: 'auto',
-              marginRight: 'auto', 
-              width: '600px',
+          <div style={{
+              width: '100%',
               minWidth: '10',
-              paddingTop:200
-                }}>
-                
+              paddingTop:10,
+              marginLeft:20
+          }}>
+            <div style={{position:'relative', float:'center', width:'100%'}}>
+              <iframe width="400" height="170"  frameborder="0" seamless  src="//www.caster.fm/widgets/em_player.php?uid=517083&c=000000&t=color"><p>Your browser does not support iframes.</p></iframe>
+            </div>
+            <div>
           <Divider hidden />
+          <div style={{position:'relative', marginTop:300}}>
+            <ReactCardCarousel spread='wide'>
+            <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Lunes
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'500px', width:'470px', minWidth:40, minHeight:40}}></img>
 
-          <Tab
-            menu={{color, inverted: true, attached: false, tabular: false }}
-            panes={panes}
-          />
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Martes
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'500px', width:'470px', minWidth:40, minHeight:40}}></img>
 
-          <FacebookProvider appId="895498000924043" style={{ }}>
-            <Comments colorScheme="light" orderBy="reverse_time" numPosts="5" href="https://www.facebook.com/Testestest-100720924932190" />
-          </FacebookProvider>
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Miercoles
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'400px', width:'570px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Jueves
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'500px', width:'470px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Viernes
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'500px', width:'470px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Sabado
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'500px', width:'470px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Domingo
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'500px', width:'470px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+            </ReactCardCarousel>
+          </div>
+            
+            <div style={{position:'relative', marginTop:600, marginLeft:'auto', marginRight:'auto', width: '50%', padding: '10px'}}>
+              <FacebookProvider appId="895498000924043" >
+                <Comments colorScheme="light" orderBy="reverse_time" numPosts="5" href="https://www.facebook.com/Testestest-100720924932190" />
+              </FacebookProvider>
+            </div>
+          </div>
         </div>
         </BrowserView>
         
         <MobileView>
-          <div style={{marginTop:0, marginLeft:0, marginRight:0, width:'100%', height:310, backgroundColor: '#E3E3E3', boxShadow:'0 4px 8px rgba(255, 231, 231, 0.6)', zIndex: 2, position: 'fixed'}}>
-            <img src={logo} alt="logo" style={{height:50, width:150, marginRight:350, marginLeft:150, minWidth:40, minHeight:40}}></img>
-            <iframe width="300" height="170px"  frameborder="0" seamless  src="//www.caster.fm/widgets/em_player.php?uid=517083&c=000000&t=color"><p>Your browser does not support iframes.</p></iframe>
-            <br></br>
-            <label style={{marginBottom:60, marginLeft:100, fontSize:40, minSize:20, fontFamily: 'gothic-open-shaded, sans-serif', fontWeight: 400, fontStyle: 'normal'}}>por siempre viernes</label>
+        <div style={{marginTop:0, marginLeft:0, marginRight:0, width:'100%', height:220, backgroundColor: '#effe05', boxShadow:'0 4px 8px rgba(255, 231, 231, 0.6)', zIndex: 2}}>
+            <img src={logo} alt="logo" style={{height:100, marginTop:0, width:250, marginRight:180, marginLeft:90, minWidth:40, minHeight:40}}></img>
+            <img src={slogan} alt="logo" style={{height:100, marginTop:0,  width:400, marginLeft: 10, marginBottom:27, minWidth: 10, minHeight:10}}></img>
           </div>
-          <div style={{ height: '600px',
-              marginLeft: 'auto',
-              marginRight: 'auto', 
-              width: '600px',
-              minWidth: '10',
-              paddingTop:200
-                }}>
-                <FacebookProvider appId="895498000924043" style={{ }}>
-            <Comments colorScheme="light" orderBy="reverse_time" numPosts="5" href="https://www.facebook.com/Testestest-100720924932190" />
-          </FacebookProvider>
+          <div style={{
+              width: '100%',
+              paddingTop:10,
+              marginLeft:20
+          }}>
+            <div style={{position:'relative', float:'center', width:'100%'}}>
+              <iframe width="300" height="170"  frameborder="0" seamless  src="//www.caster.fm/widgets/em_player.php?uid=517083&c=000000&t=color"><p>Your browser does not support iframes.</p></iframe>
+            </div>
+            <div>
+          <Divider hidden />
+          <div style={{position:'relative', marginTop:300}}>
+            <ReactCardCarousel spread='wide'>
+            <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Lunes
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'300px', width:'270px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Martes
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'300px', width:'270px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Miercoles
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'300px', width:'270px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Jueves
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'300px', width:'270px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Viernes
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'300px', width:'270px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Sabado
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'300px', width:'270px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+                  <Card style={card}>
+                    <CardContent >
+                      <Typography  variant="h5" component="h2">
+                        Domingo
+                      </Typography>
+                      <img src={astro} alt="logo" style={{height:'300px', width:'270px', minWidth:40, minHeight:40}}></img>
+
+                      <Typography  variant="h5" component="h2">
+                        Este espacio todavia no tiene horarios
+                      </Typography>
+                    </CardContent>  
+                  </Card>
+            </ReactCardCarousel>
+          </div>
+            
+            <div style={{position:'relative', marginTop:600, marginLeft:'auto', marginRight:'auto', width: '100%', padding: '10px'}}>
+              <FacebookProvider appId="895498000924043" >
+                <Comments colorScheme="light" orderBy="reverse_time" numPosts="5" href="https://www.facebook.com/Testestest-100720924932190" />
+              </FacebookProvider>
+            </div>
+          </div>
         </div>
         </MobileView>
           
@@ -192,7 +365,7 @@ class App extends Component {
 export default App;
 
 var card = {
-  backgroundColor: '#EBEBEB', 
+  backgroundColor: '#FFFFFF', 
   alignItems:'center'
 }
 
