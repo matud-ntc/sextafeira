78c622d93a8db6bc417bd9217c2c13d40590f6cf
mdalto <mdalto@altimetrik.com>
2020-06-22 19:04:03 -0700

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useRef} from 'react';
 import './App.css';
 import logo from './Images/bg.png';
 import slogan from './Images/slogan.png';
@@ -30,6 +30,8 @@ import Button from '@material-ui/core/Button';
 import ReactGA from 'react-ga';
 import Typography from '@material-ui/core/Typography';
 import  ReactCardCarousel from 'react-card-carousel';
+import wave from './Images/wave.svg'
+import ScrollToTop from './components/ScrollToTop';
 
 interface Props {
   currentView: string,
@@ -40,62 +42,83 @@ const App: React.FC<Props> = props => {
 
   ReactGA.initialize('UA-163328180-1');
   ReactGA.pageview(window.location.pathname + window.location.search);
+  
+  const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)   
+  // General scroll to element function
 
-    return(
-      <div style={{width:'100%', padding:20}}>
-        <div style={{marginLeft:10, float:'left', width:'50%', display:'flex',  justifyContent: 'left', alignItems: 'left', marginTop:10 }}>
+  const myRef = useRef(null)
+  const executeScroll = () => scrollToRef(myRef)
+  
+  return(
+    <div style={{width:'100%', overflowX: 'hidden', position:'relative'}}>
+      <div style={{width:'100%', padding:40, marginBottom:280}}>
+        <div style={{marginLeft:10, float:'left', width:'65%',display:'flex',  justifyContent: 'left', alignItems: 'left', marginTop:10 }}>
           SEXTA FEIRA
         </div> 
-        <div style={{ float:'right', width:'10%', display:'flex', justifyContent: 'right', alignItems: 'right', marginTop:10 }}>
+        <div style={rightButton}>
           <span style={{cursor:'pointer', display:'block', marginRight:10}}>Inicio</span>
+        </div> 
+        <div style={rightButton2}>
+          <span style={{cursor:'pointer', display:'block', marginRight:10}} onClick={executeScroll}>Comentarios</span>
+        </div>
+          <div style={rightButton3}>
           <span style={{cursor:'pointer', display:'block', marginRight:10}}>Noticias</span>
+        </div> 
+        <div style={rightButton4}>
           <span style={{cursor:'pointer', display:'block', marginRight:10}}>Interactivo</span>
         </div> 
-          <div style={{marginLeft:10, float:'left', width:'100%', display:'flex',  justifyContent: 'left', alignItems: 'left', marginTop:10 }}>
+        <div style={{width:'100%', display:'flex',  justifyContent: 'center', alignItems: 'center'}}>
+            <img src={wave}></img>           
+        </div>
+        <div style={{width:'100%', display:'flex',  justifyContent: 'center', alignItems: 'center', marginTop:10 }}>
+            <span style={subtitleStyle}>Por Siempre</span>
+        </div>
+        <div style={{width:'100%', display:'flex',  justifyContent: 'center', alignItems: 'center', marginTop:10 }}>
+            <span style={subtitleStyle}>VIERNES</span>
+        </div>
+        <div style={{ width:'100%', display:'flex',  justifyContent: 'center', alignItems: 'center', marginTop:60 }}>
+            <iframe height='170px' seamless src='//www.caster.fm/widgets/em_player.php?jsinit=true&uid=517083&t=blue&c=&embed=true'></iframe>
+          </div>
+          <div style={{ width:'100%', display:'flex',  justifyContent: 'center', alignItems: 'center' }}>
             <span style={share}>Compartir</span>
           </div>
-          <div style={{marginLeft:10, float:'left', width:'100%', display:'flex',  justifyContent: 'left', alignItems: 'left', marginTop:10 }}>
-          <FacebookShareButton
-            url="https://sextafeirabu.com"
-            quote="Opa, sextafeira esta en vivo"
-            className="Demo__some-network__share-button"
-          >
-            <FacebookIcon size={32} round />
-          </FacebookShareButton>     
-          <TwitterShareButton
-            url="https://sextafeirabu.com"
-            title="Opa, sextafeira esta en vivo"
-            className="Demo__some-network__share-button"
-          >
-            <TwitterIcon size={32} round />
-          </TwitterShareButton>         
-          <WhatsappShareButton
-            url="https://sextafeirabu.com"
-            title="Opa, sextafeira esta en vivo"
-            className="Demo__some-network__share-button"
-          >
-            <WhatsappIcon size={32} round />
-          </WhatsappShareButton>         
-        </div> 
-        <div style={{marginLeft:10, float:'left', width:'50%', display:'flex',  justifyContent: 'center', alignItems: 'center', marginTop:10 }}>
-          <span style={subtitleStyle}>Por Siempre VIERNES</span>
-        </div>
-        <div style={{marginLeft:10, float:'right', width:'15%', display:'flex',  justifyContent: 'center', alignItems: 'center', marginTop:150 }}>
-        <iframe height='170px' seamless src='//www.caster.fm/widgets/em_player.php?jsinit=true&uid=517083&t=blue&c=&embed=true'></iframe>
-        </div>
-        <div style={{marginLeft:10, float:'left', width:'100%', display:'flex',  justifyContent: 'left', alignItems: 'left', marginTop:10 }}>
-          <div style={{marginLeft:10, float:'left', width:'15%', display:'flex',  justifyContent: 'left', alignItems: 'left', marginTop:10 }}>
-            <span style={share}>Una radio tranqui, sin horarios.</span>
-            </div>
-        </div>
-        <div style={{marginLeft:10, float:'left', width:'100%', display:'flex',  justifyContent: 'left', alignItems: 'left' }}>
-          <div style={{marginLeft:10, float:'left', width:'15%', display:'flex',  justifyContent: 'left', alignItems: 'left'}}>
-            <span style={share}>Acá encontras de todo... a veces.</span>
-            </div>
-        </div>
+          <div style={{width:'100%', display:'flex',  justifyContent: 'center', alignItems: 'center' }}>
+            <FacebookShareButton
+              url="https://sextafeirabu.com"
+              quote="Opa, sextafeira esta en vivo"
+              className="Demo__some-network__share-button"
+            >
+              <FacebookIcon size={32} round />
+            </FacebookShareButton>     
+            <TwitterShareButton
+              url="https://sextafeirabu.com"
+              title="Opa, sextafeira esta en vivo"
+              className="Demo__some-network__share-button"
+            >
+              <TwitterIcon size={32} round />
+            </TwitterShareButton>         
+            <WhatsappShareButton
+              url="https://sextafeirabu.com"
+              title="Opa, sextafeira esta en vivo"
+              className="Demo__some-network__share-button"
+            >
+              <WhatsappIcon size={32} round />
+            </WhatsappShareButton>         
+          </div>           
       </div>
-    );
-  }
+      <div ref={myRef} style={{width:'100%', backgroundColor:'#2d5e92', display:'flex',  justifyContent: 'center', alignItems: 'center', marginTop:10 }}>
+        <span style={titleWhite}>Dejanos tu comentario</span>
+        </div>
+        <div ref={myRef} style={{width:'100%', backgroundColor:'#2d5e92', display:'flex',  justifyContent: 'center', alignItems: 'center'}}>
+
+        <FacebookProvider appId="895498000924043" >
+          <Comments colorScheme="dark" orderBy="reverse_time" numPosts="11" href="https://www.facebook.com/Testestest-100720924932190" />
+        </FacebookProvider>
+      </div>
+      <ScrollToTop />
+    </div>
+  );
+}
 
 export default App;
 
@@ -137,10 +160,67 @@ var compartir = {
 
 var subtitleStyle = {
   fontFamily: 'Suez One, serif',
-  fontSize: 'calc(10em + 1vw)',
+  fontSize: 'calc(4em + 1vw)',
+}
+
+var titleWhite = {
+  fontFamily: 'Suez One, serif',
+  fontSize: 'calc(2em + 1vw)',
+  color:'#FFFFFF',
+  fontWeight:400
 }
 
 var share = {
   fontFamily: 'Barlow, sans-serif',
   fontSize: 20
+}
+
+const rightButton = {
+  width: '17px',
+  marginTop: '33px',
+  marginRight: '18px',
+  position:'absolute' as 'absolute',
+  display:'inline-block',
+  top:10,
+  right:370,
+}
+
+const rightButton2 = {
+  width: '17px',
+  marginTop: '33px',
+  marginRight: '18px',
+  position:'absolute' as 'absolute',
+  display:'inline-block',
+  top:10,
+  right:290,
+}
+
+const rightButton3 = {
+  width: '17px',
+  marginTop: '33px',
+  marginRight: '18px',
+  position:'absolute' as 'absolute',
+  display:'inline-block',
+  top:10,
+  right:160,
+}
+
+const rightButton4 = {
+  width: '17px',
+  marginTop: '33px',
+  marginRight: '18px',
+  position:'absolute' as 'absolute',
+  display:'inline-block',
+  top:10,
+  right:70,
+}
+
+const right = {
+  width: '17px',
+  marginTop: '33px',
+  marginRight: '18px',
+  position:'absolute' as 'absolute',
+  top:280,
+  right:300,
+  cursor: 'pointer' as 'pointer',
 }
---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,73 @@
+import React, { Component, useState } from "react";
+
+interface Props {
+
+}
+
+const ScrollToTop: React.FC<Props> = props => {
+
+    const [is_visible, setIs_visible] = useState(false);
+    
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setIs_visible(true)
+        } else {
+            setIs_visible(false)
+        }
+    }
+    document.addEventListener("scroll", function(e) {
+        toggleVisibility();
+    });
+  
+
+    
+
+    const scrollToTop = () => {
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        });
+    }
+
+    return (
+      <div className="scroll-to-top" style={{float:'right', zIndex:1000}}>
+        {is_visible && (
+          <div onClick={() => scrollToTop()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="38.735"
+              width="33.749"
+            >
+              <g transform="translate(-18.121 -3.364)">
+                <rect
+                  ry="4.928"
+                  y="3.364"
+                  x="18.121"
+                  height="38.735"
+                  width="33.749"
+                  fill="#00f"
+                />
+                <g transform="translate(-.48 2.134)">
+                  <rect
+                    ry="4.928"
+                    y="1.229"
+                    x="18.601"
+                    height="38.735"
+                    width="33.749"
+                    fill="url(#b)"
+                  />
+                  <g fill="#ececec">
+                    <path d="M22.435 17.62l4.684 4.685 5.044-5.044v19.352h6.625V17.26l5.044 5.044 4.683-4.684-13.04-13.04z" />
+                    <path d="M22.435 17.62l4.684 4.685 5.044-5.044v19.352h6.625V17.26l5.044 5.044 4.683-4.684-13.04-13.04z" />
+                  </g>
+                </g>
+              </g>
+            </svg>
+          </div>
+        )}
+      </div>
+    );
+  
+}
+
+export default ScrollToTop;
